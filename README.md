52c2a7314d2d0fdbe921c3780b007715669fbf3b
Zach van Noppen <44442526+ZachvanNoppen@users.noreply.github.com>
2021-02-04 23:22:25 -0500

    v2

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-
 ## Echos
 
 The intention of the project was to create an environment that allowed users to use sound to communicate to each other.
 
-A single user will stand, pick up a speaker object and rotate it until it reaches a certain tone, then upon release it is pushed forward and emits that sound. Adding more users allows for music to be made together or for each other. Unfortunately, this idea proved much more difficult to implement than planned
+A single user will stand, pick up a speaker object and rotate it until it reaches a certain tone, then upon release it is pushed forward and emits that sound. Adding more users allows for music to be made together or for each other. Unfortunately, this idea proved much more difficult to implement than planned. Currently, the user can pick up a speaker, and place it back down by clicking above the stand. They can also add and remove objects.
 
-I found it extremely difficult just to get simple functions up and working. I blame it on not programming at all during my last 8 months in COOP. On top of that the learning curve for AFrame made me take significantly more time than I planned to need on this project. Just getting comfortable with programming on my own again versus along with lectures took some time. Towards the last few hours of work on the project however, I found a groove and really started to understand the framework.
+I found it extremely difficult just to get simple functions up and working. On top of that the learning curve for AFrame made me take significantly more time than I planned to need on this project. Just getting comfortable with programming on my own again versus along with lectures took some time. I learned the importance of components and just how useful they can be near the last hour or two working on the project. I will definately utilize them better in the future.
 
 The most challenging part was trying to build interactions that worked on mobile and desktop. I realized the framework doesn't necessarily have good ways to simply switch over functionality. Knowing the constraints of the framework now, it would have been better to design the interactions with object differently. I expected it to act more like the Bootstrap framework where simply changing a class or componenet would allow for perfect cross-over interaction.
 
--- public/js/controls.js
+++ public/js/controls.js
@@ -1,5 +1,5 @@
 
-
+//Starting the application
 AFRAME.registerComponent('start-experience', {
   init: function () {
     //we can't play sound on some browsers until we have some user interaction
@@ -11,38 +11,42 @@ AFRAME.registerComponent('start-experience', {
 });
 
 //function called from user-gesture click
-const startExperience = function() {
+function startExperience(){
   //hide user-gesture overlay
   document.querySelector('#user-gesture-overlay').style.display='none';
   
-  
+  //start all ambient music
+  const ambientSounds = document.querySelectorAll('.ambient-music');
+  ambientSounds.forEach(function(soundEntity) {
+    soundEntity.components.sound.playSound();
+  });
 }
 
 
+//Allowing objects to be picked up
 
 AFRAME.registerComponent('pickup', {
     schema: {
     },
 
     init: function () {
-      //let data = this.data;
         let el = this.el;  
         let active = false;
 
         el.addEventListener('click', function () {
             if(active){
-              //if()
-              test(active);
-              
-
+              //Placing back on the stand
+              const parent = document.querySelector('#viewer');
+              parent.innerHTML = '<a-entity cursor="rayOrigin:mouse;" raycaster="far:20; interval:200; objects:.interactive "></a-entity>';
+              createObject();
             }else{
-              const parent = document.querySelector('#ball_stage');
-              parent.remove(el);
-              document.getElementById('viewer').innerHTML += '<a-entity class="orb interactive" click-drag pickup position="1 0.3 -1.7" geometry="primitive:box; width:0.4;depth:0.4:height:0.2; " material="color:rgb(255, 255, 0)" shadow>' +
+
+              //Tying the object to the camera to 'pickup'
+              deleteObject();
+              document.getElementById('viewer').innerHTML += '<a-entity drag-rotate-component class="orb interactive" pickup position="1 0.3 -1.7" geometry="primitive:box; width:0.4;depth:0.4:height:0.2; " material="color:rgb(255, 255, 0)" shadow>' +
               '<a-circle material="color: black;" position="0 0.3 0.21" radius="0.08"></a-circle>' + 
               '<a-circle material="color: black;" position="0 0 0.21" radius="0.1"></a-circle>'+
               '</a-entity>';
-              test(active);
           }
 
           active = !active;
@@ -53,10 +57,7 @@ AFRAME.registerComponent('pickup', {
     }
   });
 
-  function test(t = "test"){
-      console.log(t);
-  }
-
+  //Deleting objects
   function deleteObject(){
     const allOrbs = document.getElementsByClassName('orb');
     const parent = document.querySelector('#ball_stage');
@@ -64,28 +65,25 @@ AFRAME.registerComponent('pickup', {
       for(i in allOrbs){
         parent.remove(allOrbs[i]);
       }
-        
     }
-    
 }
 
+//Creating new objects
 function createObject(){
     const parent = document.querySelector('#ball_stage');
     if(parent != null){
-        test("here");
-        let source = "'/assets/sound/a_note.mp3';"
         parent.innerHTML += 
-        '<a-entity drag-rotate-component sound="src: '+source+'" autoplay: true" class="orb interactive" click-drag pickup position="1 1 0" geometry="primitive:box; width:0.4;depth:0.4:height:0.2; " material="color:rgb(255, 255, 0)" shadow>' +
+        '<a-entity class="orb interactive" pickup position="1 1 0" geometry="primitive:box; width:0.4;depth:0.4:height:0.2; " material="color:rgb(255, 255, 0)" shadow>' +
         '<a-circle material="color: black;" position="0 0.3 0.21" radius="0.08"></a-circle>' + 
         '<a-circle material="color: black;" position="0 0 0.21" radius="0.1"></a-circle>'+
         '</a-entity>';
     }
-    
 }
 
+//Component to have the speaker object follow the mouse
 
 AFRAME.registerComponent('drag-rotate-component',{
-  schema : { speed : {default:1}},
+  schema : { speed : {default:2}},
   init : function(){
     this.ifMouseDown = false;
     this.x_cord = 0;
@@ -93,6 +91,7 @@ AFRAME.registerComponent('drag-rotate-component',{
     document.addEventListener('mousedown',this.OnDocumentMouseDown.bind(this));
     document.addEventListener('mouseup',this.OnDocumentMouseUp.bind(this));
     document.addEventListener('mousemove',this.OnDocumentMouseMove.bind(this));
+
   },
   OnDocumentMouseDown : function(event){
     this.ifMouseDown = true;
@@ -100,10 +99,12 @@ AFRAME.registerComponent('drag-rotate-component',{
     this.y_cord = event.clientY;
   },
   OnDocumentMouseUp : function(){
+    //document.getElementById('viewer').setAttribute("look-controls");
     this.ifMouseDown = false;
   },
   OnDocumentMouseMove : function(event)
   {
+    //if the mouse is moving, rotate 
     if(this.ifMouseDown)
     {
       var temp_x = event.clientX-this.x_cord;
